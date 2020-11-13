d16e8012e0dfb23a5d07bba1442058aa89413d10
Bjorn Krols <krols.bj@gmail.com>
2020-11-13 20:03:30 +0100

    Stricter typings

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,37 @@
-import { useState } from 'react';
-import { Args, SubmitPayload } from './types/use-formspark';
+import {useState} from 'react';
+import {Args, SubmitPayload} from './types/use-formspark';
 
-export const useFormspark = (args: Args) => {
-  const [submitting, setSubmitting] = useState(false);
+export const useFormspark = (args: Args): [(payload: SubmitPayload) => Promise<unknown>, boolean] => {
+    const [submitting, setSubmitting] = useState(false);
 
-  const submit = (payload: SubmitPayload) => {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
+    const submit = (payload: SubmitPayload) => {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
 
-      xhr.open('POST', `https://submit-form.com/${args.formId}`);
+            xhr.open('POST', `https://submit-form.com/${args.formId}`);
 
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.setRequestHeader('Accept', 'application/json');
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('Accept', 'application/json');
 
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-          setSubmitting(false);
-          const status = xhr.status;
-          if (status === 0 || (status >= 200 && status < 400)) {
-            resolve(xhr.response);
-          } else {
-            reject(xhr.statusText);
-          }
-        }
-      };
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    setSubmitting(false);
+                    const status = xhr.status;
+                    if (status === 0 || (status >= 200 && status < 400)) {
+                        resolve(xhr.response);
+                    } else {
+                        reject(xhr.statusText);
+                    }
+                }
+            };
 
-      setSubmitting(true);
+            setSubmitting(true);
 
-      xhr.send(JSON.stringify(payload));
-    });
-  };
+            xhr.send(JSON.stringify(payload));
+        });
+    };
 
-  return [submit, submitting] as const;
+    return [submit, submitting];
 };
 
 export * from './types/use-formspark';
